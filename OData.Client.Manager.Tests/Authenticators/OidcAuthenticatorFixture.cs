b019a576ba61afc48e2ee444c2e7204a20c61c43
Tobias Sibera <discjoggy@gmx.de>
2021-07-04 00:36:46 +0200

    NuGet updates, .NET Core 2 drop,

--- OData.Client.Manager.Tests/Authenticators/OidcAuthenticatorFixture.cs
+++ OData.Client.Manager.Tests/Authenticators/OidcAuthenticatorFixture.cs
@@ -1,49 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
 using System.Net.Http;
+using TestAuthorizationServer;
 
 namespace OData.Client.Manager.Tests.Authenticators
 {
-    public class OidcAuthenticatorFixture :
-#if NETCOREAPP3_1
-        Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactory<TestAuthorizationServer.Startup>,
-#endif
-        IDisposable
+    public class OidcAuthenticatorFixture : WebApplicationFactory<Startup>, IDisposable
     {
         private bool disposed = false;
 
-#if NETCOREAPP3_1
         public OidcAuthenticatorFixture()
         {
             ClientOptions.BaseAddress = new Uri("http://localhost:5000");
         }
-#else
-        public OidcAuthenticatorFixture()
-        {
-            var file = System.IO.Path.GetFullPath($"../../../../TestAuthorizationServer/bin/{(System.Diagnostics.Debugger.IsAttached ? "Debug" : "Release")}/netcoreapp3.1/TestAuthorizationServer.exe");
-            Xunit.Assert.True(System.IO.File.Exists(file), "File doesn't exist: " + file);
-            var process = new System.Diagnostics.Process
-            {
-                StartInfo = new System.Diagnostics.ProcessStartInfo
-                {
-                    FileName = file,
-                    Arguments = "--useurls=http://localhost:5000"
 
-                }
-            };
-            process.Start();
-            Pid = process.Id;
-        }
-
-        public int Pid { get; private set; } = -1;
-#endif
-
-#if NETCOREAPP3_1
         public HttpClient Client => CreateClient();
-#else
-        public HttpClient Client => null;
-#endif
 
-#if NETCOREAPP3_1
         protected override void Dispose(bool disposing)
         {
             if (!disposed)
@@ -57,28 +29,6 @@ namespace OData.Client.Manager.Tests.Authenticators
                 base.Dispose(disposing);
             }
         }
-#else
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposed)
-            {
-                if (disposing)
-                {
-                    if (Pid >= 0)
-                    {
-                        System.Diagnostics.Process.GetProcessById(Pid)?.Kill();
-                        Pid = -1;
-                    }
-                }
 
-                disposed = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-        }
-#endif
     }
 }
